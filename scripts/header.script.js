040e03e0939a2f6101be7cbae6314abb59c9ad2e
Igor Dias <dev.diasx@gmail.com>
2026-07-18 01:05:09 -0300

    refactor: new website look

--- scripts/header.script.js
+++ scripts/header.script.js
@@ -1,3 +1,23 @@
 const menu = () => {
-    
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("menu-active");
 }
+
+const nav = document.querySelector("nav");
+let logo = document.getElementById("logo");
+const spanMenu = document.querySelector("nav span");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 75) {
+        nav.classList.add("nav-fixed");
+        nav.classList.add("animate");
+            
+        logo.src = "/images/logo.png";
+        spanMenu.style.color = "#0b090a";                             
+    } else {
+        nav.classList.remove("nav-fixed", "animate");
+
+        logo.src = "/images/logoWhite.png";          
+        spanMenu.style.color = "#e2eafc";                             
+    }
+});
